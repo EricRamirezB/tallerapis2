41c2819cc1a9eb31524b7cc0bec8a4bc7d36304b
Eric V Ramirez Betancourt <ericr2617@gmail.com>
2018-10-10 20:15:18 -0500

    Linea base creacion de vista y controlador de publicacion

---
+++ TallerApis.Apis/Models/Publicacion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TallerApis.Apis.Models
+{
+    [Table("Publicaciones")]
+    public class Publicacion
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Usuario { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Descripcion { get; set; }
+
+        [Required]
+        public string FechaPublicacion { get; set; }
+
+        public int MeGusta { get; set; }
+
+        public int MeDiGusta { get; set; }
+
+        public int VecesCompartido { get; set; }
+
+        public bool EsPrivada { get; set; }
+
+    }
+}
+
+/*
+[Table("Producto")]
+public class Producto
+{
+    [Key]
+    public int Id { get; set; }
+
+    [MaxLength(50)]
+    [Required]
+    public string Nombre { get; set; }
+
+    [MaxLength(200)]
+    public string Descripcion { get; set; }
+
+    public int Cantidad { get; set; }
+
+    [MaxLength(80)]
+    [Required]
+    public string Imagen { get; set; }
+}
+}
+*/
---
+++ TallerApis.Apis/Models/PublicacionContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TallerApis.Apis.Models
+{
+    public class PublicacionContext : DbContext
+    {
+        public PublicacionContext() :base("MiPublicacionConnection")
+        {
+
+        }
+
+        public DbSet<Publicacion> Publicacion { get; set; }
+
+    }
+}
+
